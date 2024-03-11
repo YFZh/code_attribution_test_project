f59c1358cd795f585f6bc3993b386a9f30b20751
Ya Fang Zhao <zoyafg@cn.ibm.com>
2024-03-11 14:41:01 +0800

    Update asignado_filter_delete.py

--- asignado_filter_delete.py
+++ asignado_filter_delete.py
@@ -1,23 +1,32 @@
-class AsignadoFilter(admin.SimpleListFilter):
-    _title = 'Asignación'
-    _parameter_name = 'asignado'
+class disp_fig(object):
 
-    def lookups(self, request, model_admin):
-        return (
-            ('sí', 'sí'),
-            ('no', 'no'),
-        )
+    """
+    Class to display video frames
 
-    def queryset(self, request, queryset):
-        value = self.value()
+    Args:
+    Accepts video frames numpy array to display using matplotlib
 
-    def queryset2(self, request, queryset):
-        value = self.value()
+    Output:
+    Displays 8 video frames using matplotlib window
+    """
 
-    @property
-    def title(self):
-        return self._title
-    
-    @property
-    def parameter_name(self):
-        return self._parameter_name
+    def __init__(self, arr):
+        self.array = arr
+        
+        # Reshape numpy video array
+        self.video = arr.reshape(1,8,152,152)
+        
+
+    def vu():
+        # Create new matplotlib figure
+        plt.figure(figsize=(20, 2))
+
+    def figure(self):
+        # Display video frames using 8 subplot
+        for i in range(8):
+            ax = plt.subplot(1, 8, i+1)
+            image = self.video[0][i]
+            plt.imshow(image)
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
